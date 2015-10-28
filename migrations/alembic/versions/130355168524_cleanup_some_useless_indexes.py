5e4ba0d037aea70eb8dc598e335cf1f901f65dc9
Vincent Lepot <vincent.lepot@canaltp.fr>
2015-10-28 16:06:54 +0100

    Remove indexes not anymore useful since consolidation is done

---
+++ migrations/alembic/versions/130355168524_cleanup_some_useless_indexes.py
@@ -0,0 +1,30 @@
+"""Cleanup some useless indexes
+
+Revision ID: 130355168524
+Revises: 466453911d1c
+Create Date: 2015-10-28 14:37:26.677732
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '130355168524'
+down_revision = '466453911d1c'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.drop_index('coverages_region_id_idx', schema='stat')
+    op.drop_index('journey_sections_from_admin_id_idx', schema='stat')
+    op.drop_index('requests_api_idx', schema='stat')
+    op.drop_index('requests_user_id_idx', schema='stat')
+    op.drop_index('requests_user_name_idx', schema='stat')
+
+
+def downgrade():
+    op.create_index('coverages_region_id_idx', 'coverages', ['region_id'], schema='stat')
+    op.create_index('journey_sections_from_admin_id_idx', 'journey_sections', ['from_admin_id'], schema='stat')
+    op.create_index('requests_api_idx', 'requests', ['api'], schema='stat')
+    op.create_index('requests_user_id_idx', 'requests', ['user_id'], schema='stat')
+    op.create_index('requests_user_name_idx', 'requests', ['user_name'], schema='stat')
